99db6c89a58f070a3802cbcc4204d778a9f03607
Vex0on <73704968+Vex0on@users.noreply.github.com>
2021-11-24 21:31:13 +0100

    Lab1 Zadania + Rozwiązania

---
+++ lab01/lab1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int liczba1 = 5;
+    int liczba2 = 14;
+    cout<< "ZADANIE 1"<<endl;
+    cout<<endl;
+    cout<< "liczba pierwsza = " << liczba1<<endl;
+    cout<< "liczba druga = " << liczba2<<endl;
+    cout<< "Suma liczb = " << liczba1 + liczba2<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"ZADANIE 2"<<endl;
+    cout<<endl;
+    cout<< "Rozmiar int: " << sizeof(int)<<endl;
+    cout<< "Rozmiar char: " << sizeof(char)<<endl;
+    cout<< "Rozmiar float: " << sizeof(float)<<endl;
+    cout<< "Rozmiar double: " << sizeof(double)<<endl;
+    return 0;
+}
